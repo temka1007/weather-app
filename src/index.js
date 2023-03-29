8225699942067e325a6dc7139f82051dea38035d
Munkhtemuulen <munhtemuulen.kh2002@gmail.com>
2023-03-29 13:59:32 +0800

    ui completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./index.css";
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty("--rotation", rotationRatio * 360)
+}
+
+function setClock() {
+  const currentDate = new Date();
+  const secondRatio = currentDate.getSeconds() / 60;
+  const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+  const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondRatio)
+  setRotation(minuteHand, minuteRatio)
+  setRotation(hourHand, hourRatio)
+}
+
+setInterval(setClock, 1000);
+
+setClock()
+
+async function getWeaterData() {
+    const weather = await fetch("http://api.weatherapi.com/v1/current.json?key=a1fd7a18c7df45cd9cf15211232403&q=London&aqi=yes", {
+        mode: "cors"
+    })
+    const data = await weather.json()
+
+    console.log(data)
+}
+
+getWeaterData()
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: 'assets/[hash][ext][query]',
+    assetModuleFilename: "assets/[hash][ext][query]",
   },
   module: {
     rules: [
@@ -17,11 +17,15 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
       },
     ],
   },
